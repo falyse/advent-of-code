d1b6e5b39fe6ba356a5bdf58a9ce4f7dd8875617
Courtney Fricano <courtney.fricano@analog.com>
2019-12-06 11:20:27 -0500

    Refactoring

--- 2019/06/main.py
+++ 2019/06/main.py
@@ -1,14 +1,5 @@
-def process_input(input):
-    orbits = {}
-    for x in input:
-        x = x.strip()
-        a, b = x.split(')')
-        orbits[b] = a
-    # print(orbits)
-    return orbits
-
-
 class Node:
+    """Class to define a node in the orbit tree"""
     def __init__(self, name, depth):
         self.name = name
         self.parent = None
@@ -28,17 +19,30 @@ class Node:
     def get_total_depth(self):
         return self.depth + sum([x.get_total_depth() for x in self.children])
 
-    def get_node(self, name):
+    def find_node(self, name):
+        """Find descendant node with matching name"""
         if self.name == name:
             return self
         for child in self.children:
-            node = child.get_node(name)
+            node = child.find_node(name)
             if node:
                 return node
         return None
 
 
+def process_input(input):
+    """Parse each orbit definition and return data as a dict
+       where key is the orbiter (ie. key orbits value)"""
+    orbits = {}
+    for line in input:
+        a, b = line.strip().split(')')
+        orbits[b] = a
+    # print(orbits)
+    return orbits
+
+
 def create_tree(orbits, start='COM', depth=0):
+    """Construct a tree based on the connections defined in the orbits hash"""
     node = Node(start, depth)
     for k,v in orbits.items():
         if v == start:
@@ -47,7 +51,8 @@ def create_tree(orbits, start='COM', depth=0):
     return node
 
 
-def get_ancestors(tree, node):
+def get_ancestors(node):
+    """Return a list of all parent ancestors from the specified node to the tree root"""
     ancestors = []
     while True:
         parent = node.parent
@@ -59,9 +64,10 @@ def get_ancestors(tree, node):
     return ancestors
 
 
-def find_common_ancestor(tree, node0, node1):
-    ancestors0 = get_ancestors(tree, node0)
-    ancestors1 = get_ancestors(tree, node1)
+def find_common_ancestor(node0, node1):
+    """Return the nearest common ancestor of two nodes"""
+    ancestors0 = get_ancestors(node0)
+    ancestors1 = get_ancestors(node1)
     path0 = list(reversed(ancestors0))
     path1 = list(reversed(ancestors1))
     common_node = None
@@ -74,17 +80,24 @@ def find_common_ancestor(tree, node0, node1):
 
 
 with open('input.txt', 'r') as f:
-# with open('test.txt', 'r') as f:
-# with open('test1.txt', 'r') as f:
+# with open('test-1.txt', 'r') as f:
+# with open('test-2.txt', 'r') as f:
     orbits = process_input(f.readlines())
     tree = create_tree(orbits)
     # print(tree)
+
+    # Part 1:
+    #   The number of orbits for each node is the tree depth
+    #   Sum all the depths to get the total number of orbits in the system
     num_orbits = tree.get_total_depth()
     print('Num orbits', num_orbits)
 
-    you = tree.get_node('YOU')
-    san = tree.get_node('SAN')
-    ancestor = find_common_ancestor(tree, you, san)
+    # Part 2:
+    #   The minimum number of transfers between YOU and SAN
+    #   is the sum of the distances from each node to the nearest common ancestor
+    you = tree.find_node('YOU')
+    san = tree.find_node('SAN')
+    ancestor = find_common_ancestor(you, san)
     num_transfers = (you.depth - ancestor.depth - 1) + (san.depth - ancestor.depth - 1)
     print('Num transfers', num_transfers)
 
