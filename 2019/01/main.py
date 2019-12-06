b12160db5ac4834bb3f6405f126a1c2a0350a5df
Courtney Fricano <courtney.fricano@analog.com>
2019-12-06 12:53:51 -0500

    Refactoring

--- 2019/01/main.py
+++ 2019/01/main.py
@@ -1,19 +1,39 @@
-def get_mass(value):
-    if value < 0:
+def calc_fuel(mass, recurse):
+    m = int(mass / 3) - 2
+    if m <= 0:
         return 0
-    m = int(value / 3) - 2
-    # f = get_mass(m)
-    # print(m)
-    if m < 0:
-        return 0
-    if m > 0:
-        m += get_mass(m)
+    if recurse:
+        m += calc_fuel(m, recurse)
     return m
 
+
+def get_total_fuel(masses, recurse):
+    total = sum([calc_fuel(m, recurse) for m in masses])
+    print('Total fuel', total)
+    return total
+
+
+def test():
+    # No recursion
+    assert calc_fuel(12, False) == 2
+    assert calc_fuel(14, False) == 2
+    assert calc_fuel(1969, False) == 654
+    assert calc_fuel(100756, False) == 33583
+    # With recursion
+    assert calc_fuel(14, True) == 2
+    assert calc_fuel(1969, True) == 966
+    assert calc_fuel(100756, True) == 50346
+
+
+test()
+
 with open('input.txt', 'r') as f:
-    input = f.readlines()
-    # print(get_mass(14))
-    # print(get_mass(1969))
-    # print(get_mass(100756))
-    s = sum([get_mass(int(x)) for x in input])
-    print(s)
+    masses = [int(x) for x in f.readlines()]
+
+    # Part 1: No recursion
+    total = get_total_fuel(masses, False)
+    assert total == 3455717
+
+    # Part 2: With recursion
+    total = get_total_fuel(masses, True)
+    assert total == 5180690
