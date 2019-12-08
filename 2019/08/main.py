5b32b16836b8013b3676f2a2bd0aa0b7dec952f0
Courtney Fricano <falyse@gmail.com>
2019-12-08 00:51:43 -0500

    Refactoring

--- 2019/08/main.py
+++ 2019/08/main.py
@@ -1,6 +1,3 @@
-width = 25
-height = 6
-
 def process_data(input, width, height):
     layers = []
     while len(input) > 0:
@@ -8,45 +5,40 @@ def process_data(input, width, height):
         for h in range(height):
             row = input[0:width]
             input = input[width:]
-            print('row', row)
-            print('input', input)
             layer.append(row)
         layers.append(layer)
-    print(layers)
     return layers
 
-def count_zeros(layers):
-    print('asdf0')
+
+def count_digits(layers):
     min = None
     val = None
-    for l in layers:
+    for layer in layers:
         num_zeros = 0
         num_ones = 0
         num_twos = 0
-        for row in l:
-            num_zeros += sum([1 for x in row if x == '0'])
-            num_ones += sum([1 for x in row if x == '1'])
-            num_twos += sum([1 for x in row if x == '2'])
-        print('zeros', num_zeros, num_ones, num_twos)
+        for row in layer:
+            num_zeros += row.count('0')
+            num_ones += row.count('1')
+            num_twos += row.count('2')
         if min is None or num_zeros < min:
             min = num_zeros
             val = num_ones * num_twos
-    print(val)
+    return val
 
 
 def merge(layers, width, height):
     image = [['2' for x in range(width)] for y in range(height)]
-    print(image)
-    for r in range(height):
-        for c in range(width):
+    for y in range(height):
+        for x in range(width):
             for l in layers:
-                if l[r][c] != '2' and image[r][c] == '2':
-                    image[r][c] = l[r][c]
-    print(image)
+                if l[y][x] != '2' and image[y][x] == '2':
+                    image[y][x] = l[y][x]
     return image
 
 
 def render(image):
+    print()
     for row in image:
         text = ''.join(row)
         text = text.replace('0', ' ')
@@ -57,18 +49,28 @@ def render(image):
 def test():
     # Part 1
     layers = process_data('123456789012', 3, 2)
-    count_zeros(layers)
-    # assert layers[0] == [123, 456]
+    assert layers == [['123', '456'], ['789', '012']]
+    assert count_digits(layers) == 1
     # Part 2
-    # layers = process_data('0222112222120000', 2, 2)
-    # image = merge(layers, 2, 2)
+    layers = process_data('0222112222120000', 2, 2)
+    image = merge(layers, 2, 2)
+    assert image == [['0', '1'], ['1', '0']]
 
 
 test()
-exit(0)
 
 with open('input.txt', 'r') as f:
+    width = 25
+    height = 6
+
     input = f.read().strip()
     layers = process_data(input, width, height)
+
+    # Part 1
+    digit_val = count_digits(layers)
+    print('Digit check:', digit_val)
+    assert digit_val == 1224
+
+    # Part 2
     image = merge(layers, width, height)
     render(image)
