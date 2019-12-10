f666501c18ad5d6267ac2fe3e0d349aa2b5c4c0a
Courtney Fricano <courtney.fricano@analog.com>
2019-12-10 11:54:42 -0500

    Refactoring

--- 2019/10/main.py
+++ 2019/10/main.py
@@ -1,7 +1,5 @@
 import sys
-sys.path.append('../intcode')
 sys.path.append('..')
-from intcode import IntcodeComputer
 import util
 
 from itertools import permutations
@@ -36,43 +34,24 @@ def process_grid(grid):
             coords = []
             if grid[y][x]:
                 # print('Asteroid at', x, y)
-                # deltas = range(-4,5)
                 combos = get_combos(len(line))
                 for dx,dy in combos:
-                # for dx in deltas:
-                #     for dy in deltas:
-                #     if ((dx != dy or dx == 1) and
-                #             (dx != 0 or dy == 1) and
-                #             (dy != 0 or dx == 1)):
                     coord = search_path(grid, x, y, dx, dy)
                     if coord is not None:
                         cnt += 1
                         if x == 11 and y == 13:
                             print('asdf1', coord)
                         coords.append(coord)
-                # if search_path(grid, x, y, 3, -1):
-                #     cnt += 1
-                # if search_path(grid, x, y, 2, -1):
-                #     cnt += 1
-                # if search_path(grid, x, y, 1, -1):
-                #     cnt += 1
                 cnts[y][x] = cnt
                 if cnt > max:
                     max_coords = (x,y)
                     max = cnt
                     clist = coords.copy()
-                if x == y == 0:
-                    print('Count 0,0:', cnt)
     print(cnts)
-    # render(match)
-    # for c in coords:
-    #     print(c)
-    # exit(1)
     return cnts, max_coords, max, clist
 
 def get_combos(max):
     combos = get_offset_combos(1, 0)
-    # combos.extend(get_offset_combos(2, 1))
     for i in range(max):
         combos.extend([(-1*i,-1), (-1*i,1), (i,-1), (i,1)])
         combos.extend([(-1,-1*i), (1,-1*i), (-1,i), (1,i)])
@@ -92,7 +71,6 @@ def get_combos(max):
         if ok:
             valid.append((x,y))
     valid = set(valid)
-    # print(valid)
     return valid
 
 def get_offset_combos(offset, mid):
@@ -105,11 +83,6 @@ def get_offset_combos(offset, mid):
     return list(combos)
 
 def search_path(grid, x, y, delta_x, delta_y):
-    # if (delta_x == delta_y == 0 or (delta_x == 0 and delta_y == 2) or (delta_x == 2 and delta_y == 0) or
-    #     (delta_x == 0 and delta_y == -2) or (delta_x == -2 and delta_y == 0) or
-    #     delta_x == delta_y == 2 or delta_x == delta_y == -2):
-    #     return False
-    # print('  search path', delta_x, delta_y)
     debug = x == 11 and y == 13
     while True:
         x += delta_x
@@ -130,14 +103,8 @@ def search_path(grid, x, y, delta_x, delta_y):
 def get_angle(coord):
     x,y = coord
     angle = math.atan2(-1*y, x) * 180/math.pi
-    # angle = math.atan2(y, x) * 180/math.pi
-    # print(x, y, angle)
-    # angle = 360 - angle
-    # angle -= 90
-    # if angle < 0:
-    #     angle += 360
+    # Convert to clockwise angle
     angle = (90 - angle) % 360
-    # print(' ', x, y, angle)
     return angle
 
 def test():
@@ -161,44 +128,30 @@ with open('input.txt', 'r') as f:
     grid = util.make_grid(size, size, fill=False)
     for y,line in enumerate(input):
         for x,char in enumerate(line):
-            # print('xy', x, y, 'char', char)
             grid[y][x] = char != '.'
-    # render(grid)
 
     counts,max_coords, max_count,coords = process_grid(grid)
     print('Max', max_count, 'at', max_coords, 'coords', coords)
+    assert max_count == 326
 
-    for c in coords:
-        print(c)
-    # for x,y,angle in coords:
-    #     if x == 11:
-    #         print('asdf', x, y, angle)
-    print()
-    # exit(1)
-
+    # Part 2
     step_max = 200
     # step_max = 18+9+9
     step = 0
     while True:
         sorted_coords = []
         coords = sorted(coords, key=lambda x: x[2])
-        print('coords len', len(coords))
         render(grid)
         for x,y,angle in coords:
             step += 1
-            if step == step_max:
-                print('Reached %0d: ' % step_max, x, y)
-                print('  ', x*100 + y)
-                exit(0)
             grid[y][x] = False
             print('Step', step, 'vaporized', x, y, 'angle', angle)
+            if step == step_max:
+                print('Reached step %0d: ' % step_max, x, y)
+                value = x*100 + y
+                print('  ', value)
+                assert value == 1623
+                exit(0)
         coords = vaporize_grid(grid, max_coords)
-
-
-# 2012 wrong
-# 3119 wrong
-# 3018 wrong
-# 1625 wrong
-# 1708 wrong
 
 
