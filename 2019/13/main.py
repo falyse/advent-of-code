3bd478854113f11cf12c22f6a2f5e5c5bc7ca417
Courtney Fricano <courtney.fricano@analog.com>
2019-12-13 09:22:22 -0500

    Refactoring

--- 2019/13/main.py
+++ 2019/13/main.py
@@ -6,68 +6,70 @@ import util
 from collections import deque
 
 
+def process_outputs(outputs):
+    tiles = []
+    tile = []
+    i = 0
+    for o in outputs:
+        tile.append(o)
+        if not (i+1) % 3:
+            tiles.append(tile)
+            tile = []
+        i += 1
+    # print(items)
+    return tiles
+
+
 with open('input.txt', 'r') as f:
     program_code = [int(x) for x in f.read().split(',')]
     computer = IntcodeComputer(debug=False)
-
-    program_code[0] = 2
     inputs = deque()
+
+    # Part 1
+    computer.run(program_code, inputs)
+    outputs = computer.outputs
+    tiles = process_outputs(outputs)
+    num_blocks = sum([1 for x in tiles if x[2] == 2])
+    print('Num blocks:', num_blocks)
+    assert num_blocks == 344
+
+    # Part 2
+    program_code[0] = 2
     computer.initialize(program_code, inputs)
 
-    j = 0
-    dir = 1
-    num_blocks = 344
-    first = True
-    last_paddle = (0,0)
-    last_ball = (0,0)
     score = 0
     while True:
         computer.reset_outputs()
-        inputs.append(dir)
-        print('inputs', inputs)
         done = computer.execute()
         outputs = computer.outputs
         print('outputs', outputs)
 
-        items = []
-        item = []
-        i = 0
-        for o in outputs:
-            item.append(o)
-            if not (i+1) % 3:
-                items.append(item)
-                item = []
-            i += 1
-        # print(items)
+        tiles = process_outputs(outputs)
 
-        for x, y, id in items:
+        for x, y, id in tiles:
             if x == -1 and y == 0:
-                print('Display score:', id)
                 score = id
-            if id == 0 and not first and (x,y) != last_ball and (x,y) != last_paddle:
-                print('# Destroyed block at', x, y)
-                num_blocks -= 1
+                if score > 0:
+                    num_blocks -= 1
+                print('Display score:', score, '- Remaining blocks:', num_blocks)
             if id == 3:
                 print('* Paddle at', x, y)
-                last_paddle = (x,y)
                 xp = x
             if id == 4:
                 print('Ball at', x, y)
-                last_ball = (x,y)
                 xb = x
-        first = False
-        if xb > xp:
-            dir = 1
-            # inputs.append(1)
-        elif xb < xp:
-            dir = -1
-            # inputs.append(-1)
-        else:
-            dir = 0
-            # inputs.append(0)
 
-        print('Num blocks:', num_blocks)
+        # Move paddle towards ball
+        if xb > xp:
+            inputs.append(1)
+        elif xb < xp:
+            inputs.append(-1)
+        else:
+            inputs.append(0)
+
         if num_blocks <= 0:
-            assert score == 17336
-            exit(0)
+            break
+
+    print('Final score:', score)
+    assert score == 17336
 
