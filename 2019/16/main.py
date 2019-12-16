e3c02871a0cb2e1456eaf6b330729ad19b07fe37
Courtney Fricano <courtney.fricano@analog.com>
2019-12-16 13:02:33 -0500

    Day 16 part 2

--- 2019/16/main.py
+++ 2019/16/main.py
@@ -7,11 +7,14 @@ import math
 base_pattern = np.array([0, 1, 0, -1])
 pattern_cache = {}
 
-def decode_signal(input, num_phases):
-    offset = input[0:7]
+def decode_signal(input, num_phases, repeat=10000, override_offset=None):
+    offset = int(input[0:7])
+    if override_offset is not None:
+        offset = override_offset
     print('offset', offset)
-    input = input * 10000
-    result = run_fft(input, num_phases)
+    input = input * repeat
+    input = input[offset:]
+    result = run_fft(input, num_phases, offset)
     return result
 
 
@@ -20,31 +23,48 @@ def run_fft(input, num_phases, offset=0):
     pattern_cache = {}
     digits = np.array([int(x) for x in input.strip()])
     for i in range(num_phases):
-        digits = calc_fft(digits)
-        if not i % 10:
-            print('Finished iteration', i)
+        print('Starting phase', i)
+        # digits = calc_fft_accel(digits, offset)
+        digits = run_phase(digits)
+        # if not i % 10:
+        #     print('Finished phase', i)
     result = ''.join([str(x) for x in digits])
-    result = result[offset:offset+8]
+    # Extract result digits at offset
+    result = result[0:8]
     print('result', result)
     return result
 
 
-def calc_fft(digits):
+def run_phase(digits):
+    outputs = []
+    val = 0
+    for i,d in enumerate(reversed(digits)):
+        val += d
+        outputs.append(ones_digit(abs(val)))
+        # print(outputs)
+    outputs = list(reversed(outputs))
+    print('outputs', outputs)
+    return outputs
+
+
+# def calc_fft_accel(digits, offset):
+#     # print('digits ', digits)
+#     outputs = [ones_digit(abs(sum(digits[j:]))) for j in range(len(digits))]
+#     print('outputs', outputs)
+#     return outputs
+
+
+def calc_fft_accel(digits, offset):
     # print('digits ', digits)
     outputs = []
-    for i, digit in enumerate(digits):
-        pattern = get_full_pattern(i+1, len(digits))
-        # print('pattern', pattern)
-        mult = digits * pattern
-        # print('mult', mult)
-        output = abs(sum(mult))
-        output = ones_digit(output)
-        outputs.append(output)
-        # print(output)
-        if not i % 100 and i > 0:
-            print('Finished digit', i)
-    # print(outputs)
-    return np.array(outputs)
+    for j in range(len(digits)):
+        vals = digits[j:]
+        total = abs(sum(vals))
+        print('  j', j, ':', vals, ' :  sum', total)
+        outputs.append(ones_digit(total))
+    print('results', outputs)
+    return outputs
+
 
 def get_full_pattern(i, length):
     if i not in pattern_cache:
@@ -75,11 +95,29 @@ def test():
 # test()
 
 def test2():
+    assert run_fft('1234'*1, 1, offset=0) == '2574'
+    assert run_fft('1234'*2, 1, offset=0) == '40800974'
+    # assert run_fft('1234'*3, 1, offset=0) == '65261574'
+
+    assert decode_signal('1234', 1, repeat=1, override_offset=0, accel=False) == '2574'
+    assert decode_signal('1234', 1, repeat=2, override_offset=0, accel=False) == '40800974'
+    exit(0)
+# test2()
+
+def test3():
+    # assert decode_signal('1234', 1, repeat=5, override_offset=12) == '09740974'
+    # assert decode_signal('1234', 2, repeat=5, override_offset=12) == '00140014'
+    # assert decode_signal('1234', 2, repeat=5, override_offset=11) == '40014001'
+
     assert decode_signal('03036732577212944063491565474664', 100) == '84462026'
     exit(0)
-test2()
+# test3()
 
 
 with open('input.txt', 'r') as f:
-    input = f.read()
-    run_fft(input, 100)
+    input = f.read().strip()
+    # run_fft(input, 100)
+    decode_signal(input, 100)
+
+
+# Fork from main-d
