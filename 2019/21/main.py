c39823a420c812346899e0953bf840c952c0f966
Courtney Fricano <falyse@gmail.com>
2019-12-21 00:23:37 -0500

    Started Day 21

---
+++ 2019/21/main.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('../intcode')
+sys.path.append('..')
+from intcode import IntcodeComputer
+from collections import deque
+import util
+
+
+def test():
+    pass
+
+test()
+
+
+with open('input.txt', 'r') as f:
+    input = f.readlines()
+    # program_code = [int(x) for x in f.read().split(',')]
+    # computer = IntcodeComputer(debug=False)
+    # inputs = deque()
+    # computer.run(program_code, inputs)
