9b07ff816b160fc845000b3e4de680f61d87ede7
Courtney Fricano <courtney.fricano@analog.com>
2020-11-30 16:34:45 -0500

    2016 day 9 part 1

--- 2016/09/main.py
+++ 2016/09/main.py
@@ -4,49 +4,60 @@ import util
 
 
 def process(input):
-    item = ''
     in_paren = False
+    in_marker = False
+    marker = ''
+    num_chars = -1
     data = []
-    for x in input:
+    for i, x in enumerate(input):
+        if num_chars > 0:
+            data.append(x)
+            num_chars -= 1
+            continue
+
         if x == '(':
             in_paren = True
-            item = ''
-        elif x == ')':
-            in_paren = False
-            data.append(item)
-        elif in_paren:
-            item += x
+            if not in_marker:
+                in_marker = True
+
+        if in_marker:
+            marker += x
         else:
             data.append(x)
+
+        if x == ')':
+            in_paren = False
+            if in_marker:
+                data.append(marker)
+                num_chars, repeat = util.ints(marker)
+                in_marker = False
+                marker = ''
+
     print(data)
 
     output = ''
-    num_chars = 1
+    num_chars = -1
     char_cnt = 0
-    group = ''
-    repeat = 1
-    in_marker = False
-    for item in data:
-        if in_marker == False and len(item) > 1:
-            in_marker = True
-            num_chars, repeat = item.split('x')
-            num_chars = int(num_chars)
-            repeat = int(repeat)
-
-        char_cnt += 1
-        group += item
-
-        elif char_cnt == num_chars:
-            for x in range(repeat):
-                output += group
-                in_marker = False
+    char_group = ''
+    for x in data:
+        if len(x) > 1:
+            num_chars, repeat = util.ints(x)
             char_cnt = 0
-            repeat = 1
-            group = ''
+            char_group = ''
+        elif num_chars > char_cnt:
+            char_group += x
+            char_cnt += 1
+            if num_chars == char_cnt:
+                for i in range(repeat):
+                    output += char_group
+                num_chars = -1
+        else:
+            output += x
 
     print(output)
-
+    print()
     return output
+
 
 def test():
     assert(process('ADVENT') == 'ADVENT')
@@ -55,11 +66,12 @@ def test():
     assert(process('A(2x2)BCD(2x2)EFG') == 'ABCBCDEFEFG')
     assert(process('(6x1)(1x3)A') == '(1x3)A')
     assert(process('X(8x2)(3x3)ABCY') == 'X(3x3)ABC(3x3)ABCY')
+    exit(0)
 
-test()
+# test()
 
 
 with open('input.txt', 'r') as f:
-    input = f.read().strip().splitlines()
+    input = f.read().strip()
     output = process(input)
-    print(output)
+    print(len(output))
