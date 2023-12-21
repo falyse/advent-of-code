355d813a7bdc19e71d85ab147fa22a11edcbbf1c
Courtney Fricano <falyse@gmail.com>
2023-12-20 20:10:16 -0500

    2023 day 9

---
+++ 2023/09/main_part2.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.append('../..')
+import util
+
+
+def process(input):
+    lines = input.strip().splitlines()
+    histories = [util.ints(x) for x in lines]
+    print()
+    print(histories)
+    total = 0
+    for h in histories:
+        val = extrapolate(h)
+        print('Next val', val)
+        total += val
+    print('Total', total)
+    return total
+
+def diff_list(nums):
+    diff_list = []
+    for x, y in zip(nums[0::], nums[1::]):
+        diff_list.append(y-x)
+    return diff_list
+
+def extrapolate(nums):
+    lists = [nums]
+    while not all([x == 0 for x in nums]):
+        nums = diff_list(nums)
+        lists.append(nums)
+    print(lists)
+    
+    lists = list(reversed(lists))
+    c = lists.copy()
+
+    for i, nums in enumerate(lists):
+        print(i, nums)
+        if i == 0:
+            val = 0
+        else:
+            val = nums[0] - c[i-1][0]
+        if i == len(lists)-1:
+            return val
+        c[i].insert(0, val)
+
+
+def test():
+    assert(extrapolate([10,13,16,21,30,45]) == 5)
+
+    test_input = '''
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+
+    '''
+    assert(process(test_input) == 2)
+
+test()
+# exit(0)
+
+
+with open('input.txt', 'r') as f:
+    input = f.read()
+    val = process(input)
+    print('Result:', val)
